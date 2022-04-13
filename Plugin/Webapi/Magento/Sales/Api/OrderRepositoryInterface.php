d0c4b0d8978be20972339a3eb220c1c9c7b00aad
Roman Hutterer <roman204@users.noreply.github.com>
2022-04-13 16:00:47 +0200

    Update OrderRepositoryInterface.php
    add a real check if storeCode is given in request

--- Plugin/Webapi/Magento/Sales/Api/OrderRepositoryInterface.php
+++ Plugin/Webapi/Magento/Sales/Api/OrderRepositoryInterface.php
@@ -12,6 +12,7 @@ class OrderRepositoryInterface
     private \Magento\Store\Model\StoreManagerInterface $storeManager;
     private \Magento\Framework\Api\Search\FilterGroup $filterGroup;
     private \Magento\Framework\Api\Filter $filter;
+    private \Magento\Framework\App\RequestInterface $request;
 
     /**
      * @param \Magento\Store\Model\StoreManagerInterface $storeManager
@@ -21,25 +22,46 @@ class OrderRepositoryInterface
     public function __construct(
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Framework\Api\Search\FilterGroup $filterGroup,
-        \Magento\Framework\Api\Filter $filter
+        \Magento\Framework\Api\Filter $filter,
+        \Magento\Framework\App\RequestInterface $request
     ) {
         $this->storeManager = $storeManager;
         $this->filterGroup = $filterGroup;
         $this->filter = $filter;
+        $this->request = $request;
     }
 
     public function beforeGetList(
         \Magento\Sales\Api\OrderRepositoryInterface $subject,
         \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
     ) {
-        $this->filter->setField('store_id');
-        $this->filter->setConditionType('eq');
-        $this->filter->setValue($this->storeManager->getStore()->getId());
-        $storeFilterGroup = $this->filterGroup->setFilters([$this->filter]);
-        $groups = $searchCriteria->getFilterGroups();
-        $groups[] = $storeFilterGroup;
-        $searchCriteria->setFilterGroups($groups);
+        $pathParts = $this->stripPathBeforeStorecode($this->request->getPathInfo());
+        $storeCode = current($pathParts);
+        $stores = $this->storeManager->getStores(false, true);
+        if (isset($stores[$storeCode])) {
+            $this->filter->setField('store_id');
+            $this->filter->setConditionType('eq');
+            $this->filter->setValue($this->storeManager->getStore()->getId());
+            $storeFilterGroup = $this->filterGroup->setFilters([$this->filter]);
+            $groups = $searchCriteria->getFilterGroups();
+            $groups[] = $storeFilterGroup;
+            $searchCriteria->setFilterGroups($groups);
+        }
         return [$searchCriteria];
+    }
+
+    /**
+     * Process path
+     *
+     * @param string $pathInfo
+     * @return array
+     */
+    private function stripPathBeforeStorecode($pathInfo)
+    {
+        $pathParts = explode('/', trim($pathInfo, '/'));
+        array_shift($pathParts);
+        $path = '/' . implode('/', $pathParts);
+        return explode('/', ltrim($path, '/'), 2);
     }
 }
 
